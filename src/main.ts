927a84f7be40a668d65483d790f35d5500690d43
us/ashelestiuk <ashelestiuk@deloitte.com>
2021-04-21 09:36:24 +0300

    add aws-exports

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
-Auth.configure(aws_exports);
 
 if (environment.production) {
   enableProdMode();
